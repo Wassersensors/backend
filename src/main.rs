6b08bc36f64e2ccac003196824cc97eb41e3517d
Peter Squicciarini <stripedpajamas273@gmail.com>
2022-10-29 22:39:13 -0700

    Add logging and dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use backend::{data, filters};
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+
     let db = data::blank_db();
     let api = filters::records(db);
 
-    warp::serve(api)
-        .run(([127, 0, 0, 1], 42069))
+    let cors = warp::cors().allow_any_origin();
+    let log = warp::log("backend");
+
+    let routes = api.with(log);
+    let routes = routes.with(cors);
+
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], 42069))
         .await;
 }
