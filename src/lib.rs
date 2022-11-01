72e94b83bca56894fcf640ad686f1cc7d1a0dee2
Joel Wasserman <joel.wasserman8@gmail.com>
2022-11-01 13:41:10 -0700

    return multi device record support (#10)

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,15 @@ pub mod util {
 pub mod data {
     use serde::{Deserialize, Serialize};
     use std::{sync::Arc, collections::VecDeque};
+    use std::collections::HashMap;
     use tokio::sync::Mutex;
 
     use crate::util::now;
 
-    pub type Db = Arc<Mutex<VecDeque<Record>>>;
+    pub type Db = Arc<Mutex<HashMap<String, VecDeque<Record>>>>;
 
     pub fn blank_db() -> Db {
-        Arc::new(Mutex::new(VecDeque::with_capacity(86400)))
+        Arc::new(Mutex::new(HashMap::new()))
     }
 
     #[derive(Deserialize, Serialize, Clone)]
@@ -33,6 +34,12 @@ pub mod data {
         pub timestamp: u128,
         pub rate: f32,
         pub total_volume: f32,
+    }
+
+    #[derive(Deserialize, Serialize, Clone)]
+    pub struct OutputRecord {
+        pub device_id: String,
+        pub record: Record, 
     }
 
     impl Record {
@@ -64,7 +71,7 @@ pub mod filters {
     }
 
     pub fn record_create(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        warp::path!("record")
+        warp::path!("record" / String)
         .and(warp::post())
         .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
         .and(with_db(db))
@@ -78,20 +85,24 @@ pub mod filters {
 
 pub mod handlers {
     use std::convert::Infallible;
-
+    use std::collections::VecDeque;
     use crate::util::now;
 
-    use super::data::{Db, Record, InputRecord};
+    use super::data::{Db, Record, OutputRecord, InputRecord};
 
-    pub async fn write_record(input: InputRecord, db: Db) -> Result<impl warp::Reply, Infallible> {
-        let mut vec = db.lock().await;
+    pub async fn write_record(device_id: String, input: InputRecord, db: Db) -> Result<impl warp::Reply, Infallible> {
+        let mut map = db.lock().await;
         let record = Record {
             timestamp: now(),
             rate: input.rate,
             total_volume: input.total_volume,
         };
+        if !map.contains_key(&device_id) {
+          map.insert(device_id.clone(), VecDeque::with_capacity(86400));
+        }
+        let vec = map.get_mut(&device_id).unwrap();
         if vec.len() >= 86400 {
-            _ = vec.pop_front();
+          vec.pop_front();
         }
         vec.push_back(record.clone());
 
@@ -99,11 +110,18 @@ pub mod handlers {
     }
 
     pub async fn get_record(db: Db) -> Result<impl warp::Reply, Infallible> {
-        let records = db.lock().await;
-        let records = records.clone();
-        let default_record = Record::default();
-        let record = records.back().unwrap_or(&default_record);
+        let records_map = db.lock().await;
+        let records_map = records_map.clone();
+        let default_record = Record::default(); 
+        let mut out: Vec<OutputRecord> = Vec::new();
+        for key in records_map.keys() {
+          let output_record = OutputRecord {
+            device_id: key.clone(),
+            record: records_map.get(key).unwrap().back().unwrap_or(&default_record).clone()
+          };
+          out.push(output_record);
+        }
 
-        Ok(warp::reply::json(&record))
+        Ok(warp::reply::json(&out))
     }
 }
