4fd38baa7295526e445b973454feb7c124bfce20
Joel Wasserman <joel.wasserman8@gmail.com>
2022-11-01 15:26:04 -0700

    add alias routes

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,10 @@ pub mod data {
 
     use crate::util::now;
 
-    pub type Db = Arc<Mutex<HashMap<String, VecDeque<Record>>>>;
+    pub type Db = Arc<Mutex<(HashMap<String, String>, HashMap<String, VecDeque<Record>>)>>;
 
     pub fn blank_db() -> Db {
-        Arc::new(Mutex::new(HashMap::new()))
+        Arc::new(Mutex::new((HashMap::new(), HashMap::new())))
     }
 
     #[derive(Deserialize, Serialize, Clone)]
@@ -29,7 +29,7 @@ pub mod data {
         pub total_volume: f32,
     }
 
-    #[derive(Deserialize, Serialize, Clone)]
+    #[derive(Deserialize, Serialize, Clone, Debug)]
     pub struct Record {
         pub timestamp: u128,
         pub rate: f32,
@@ -39,7 +39,13 @@ pub mod data {
     #[derive(Deserialize, Serialize, Clone)]
     pub struct OutputRecord {
         pub device_id: String,
+        pub alias: String,
         pub record: Record, 
+    }
+
+    #[derive(Deserialize, Serialize, Clone)]
+    pub struct RegisterRequest {
+        pub alias: String,
     }
 
     impl Record {
@@ -58,9 +64,10 @@ pub mod filters {
 
     use crate::{data::Db, handlers};
 
-    pub fn records(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    pub fn routes(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         record_get(db.clone())
-            .or(record_create(db))
+            .or(record_create(db.clone()))
+            .or(register_device(db))
     }
 
     pub fn record_get(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -69,6 +76,14 @@ pub mod filters {
             .and(with_db(db))
             .and_then(handlers::get_record)
     }
+
+    pub fn register_device(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+      warp::path!("register" / String)
+          .and(warp::post())
+          .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
+          .and(with_db(db))
+          .and_then(handlers::register_device)
+  }
 
     pub fn record_create(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         warp::path!("record" / String)
@@ -88,19 +103,20 @@ pub mod handlers {
     use std::collections::VecDeque;
     use crate::util::now;
 
-    use super::data::{Db, Record, OutputRecord, InputRecord};
+    use super::data::{Db, Record, OutputRecord, RegisterRequest, InputRecord};
 
     pub async fn write_record(device_id: String, input: InputRecord, db: Db) -> Result<impl warp::Reply, Infallible> {
-        let mut map = db.lock().await;
+        let mut mutex = db.lock().await;
+        let records = &mut mutex.1;
         let record = Record {
             timestamp: now(),
             rate: input.rate,
             total_volume: input.total_volume,
         };
-        if !map.contains_key(&device_id) {
-          map.insert(device_id.clone(), VecDeque::with_capacity(86400));
+        if !records.contains_key(&device_id) {
+          records.insert(device_id.clone(), VecDeque::with_capacity(86400));
         }
-        let vec = map.get_mut(&device_id).unwrap();
+        let vec = records.get_mut(&device_id).unwrap();
         if vec.len() >= 86400 {
           vec.pop_front();
         }
@@ -109,15 +125,24 @@ pub mod handlers {
         Ok(warp::reply::json(&record))
     }
 
+    pub async fn register_device(device_id: String, input: RegisterRequest, db: Db) -> Result<impl warp::Reply, Infallible> {
+        let mut mutex = db.lock().await;
+        let devices = &mut mutex.0;
+        devices.insert(device_id.clone(), input.alias);
+
+        Ok(warp::http::StatusCode::OK)
+    }
+
     pub async fn get_record(db: Db) -> Result<impl warp::Reply, Infallible> {
-        let records_map = db.lock().await;
-        let records_map = records_map.clone();
+        let mutex = db.lock().await;
+        let (devices, records) = mutex.clone();
         let default_record = Record::default(); 
         let mut out: Vec<OutputRecord> = Vec::new();
-        for key in records_map.keys() {
+        for key in records.keys() {
           let output_record = OutputRecord {
             device_id: key.clone(),
-            record: records_map.get(key).unwrap().back().unwrap_or(&default_record).clone()
+            alias: devices.get(key).unwrap_or(key).clone(),
+            record: records.get(key).unwrap().back().unwrap_or(&default_record).clone()
           };
           out.push(output_record);
         }
