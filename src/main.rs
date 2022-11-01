268ec33e3527336ff76949e3f1960303ed14cf1d
Peter Squicciarini <squipet@amazon.com>
2022-11-01 16:15:50 -0700

    Add CORS header manually

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use backend::{data, filters};
-use warp::Filter;
+use warp::{Filter, hyper::HeaderMap, http::HeaderValue};
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
+    let mut headers = HeaderMap::new();
+    headers.insert("Access-Control-Allow-Origin", HeaderValue::from_static("*"));
+
     let db = data::blank_db();
     let api = filters::routes(db);
 
-    let cors = warp::cors().allow_any_origin();
     let log = warp::log("backend");
 
     let routes = api.with(log);
-    let routes = routes.with(cors);
+    let routes = routes.with(warp::reply::with::headers(headers));
 
     warp::serve(routes)
         .run(([0, 0, 0, 0], 42069))
