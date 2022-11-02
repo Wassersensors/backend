ebffd74330ec19eef4af620af70290c892c15423
Peter Squicciarini <squipet@amazon.com>
2022-11-02 10:11:02 -0700

    Fix cors

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use backend::{data, filters};
-use warp::{Filter, hyper::HeaderMap, http::HeaderValue};
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let mut headers = HeaderMap::new();
-    headers.insert("Access-Control-Allow-Origin", HeaderValue::from_static("*"));
-
     let db = data::blank_db();
     let api = filters::routes(db);
 
     let log = warp::log("backend");
-    let cors = warp::cors().allow_methods(vec!["GET", "POST", "OPTIONS"]);
+    let cors = warp::cors()
+        .allow_methods(vec!["GET", "POST", "OPTIONS"])
+        .allow_headers(vec!["Content-Type"])
+        .allow_any_origin();
 
     let routes = api.with(log);
-    let routes = routes.with(warp::reply::with::headers(headers));
     let routes = routes.with(cors);
 
     warp::serve(routes)
